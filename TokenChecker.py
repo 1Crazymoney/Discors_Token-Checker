8362850787720be2e5ecf4fa84360c124358eca1
Vatos <92579954+VatosV2@users.noreply.github.com>
2023-09-23 13:03:45 +0200

    Update TokenChecker.py

--- TokenChecker.py
+++ TokenChecker.py
@@ -1,4 +1,4 @@
-import requests                                                                                                                                                                                                                                                                                                               
+import requests 
 from colorama import Style, Fore
 import os
 import ctypes
